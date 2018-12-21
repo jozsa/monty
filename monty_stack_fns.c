8ee8e5eaff1fbb8e3e26b7098ad69cf8568204f2
jozsa <allisonjoyweiner@gmail.com>
2018-12-21 01:38:38 +0000

    Betty compliance

--- monty_stack_fns.c
+++ monty_stack_fns.c
@@ -31,7 +31,7 @@ void m_push(stack_t **stack, unsigned int line_number)
 void m_pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = *stack;
-	
+
 	if (*stack == NULL)
 		pop_error(line_number);
 	*stack = (*stack)->next;
